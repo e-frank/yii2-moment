e82a04ac16f6e93fa10256b8e29594d8295841ef
Elmar Frank <elmar.frank@efrank.at>
2019-09-17 16:54:59 +0200

    load only current language, otherwise fallback
    reduces library size

--- MomentAsset.php
+++ MomentAsset.php
@@ -1,13 +1,36 @@
 <?php
 namespace x1\assets\moment;
+use Yii;
 
 class MomentAsset extends \yii\web\AssetBundle
 {
-	public $sourcePath = '@vendor/moment/moment/min';
-	
-	public $js         = [
-		'moment.js' => 'moment-with-locales.min.js',
-	];
+    public $fallback   = 'en-GB';
+    public $sourcePath = '@vendor/moment/moment';
 
+    public $js = [
+        'moment.js' => 'min/moment.min.js',
+    ];
+
+    public $publishOptions = [
+        'only' => [
+            'min/moment.min.js'
+        ],
+    ];
+
+
+    public function init() {
+        $lang = strtolower(Yii::$app->language);
+        if (!file_exists(Yii::getAlias(sprintf('%s/locale/%s.js', $this->sourcePath, $lang)))) {
+            $lang = explode('-', $lang, 2)[0];
+
+            if (!file_exists(Yii::getAlias(sprintf('%s/locale/%s.js', $this->sourcePath, $lang)))) {
+                $lang = strtolower($this->fallback);
+            }
+        }
+
+        $langfile = Yii::getAlias(sprintf('locale/%s.js', $lang));
+        $this->js[$lang . '.js'] = $langfile;
+        $this->publishOptions['only'][] = $langfile;
+    }
 }
 ?>
